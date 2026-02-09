31147cb77e4106ee6fdd45dba02c1cae05e3ebc5
Manisha <manishakatika642@gmail.com>
2026-02-09 20:42:00 +0530

    Create rat_in_a_maze.py

---
+++ rat_in_a_maze.py
@@ -0,0 +1,39 @@
+class Solution:
+    def ratInMaze(self, maze):
+        n = len(maze)
+        res = []
+        path = []
+
+        def backtrack(r, c):
+            if r == n - 1 and c == n - 1:
+                res.append("".join(path))
+                return
+
+            maze[r][c] = 0
+
+            if r + 1 < n and maze[r + 1][c] == 1:
+                path.append('D')
+                backtrack(r + 1, c)
+                path.pop()
+
+            if c - 1 >= 0 and maze[r][c - 1] == 1:
+                path.append('L')
+                backtrack(r, c - 1)
+                path.pop()
+
+            if c + 1 < n and maze[r][c + 1] == 1:
+                path.append('R')
+                backtrack(r, c + 1)
+                path.pop()
+
+            if r - 1 >= 0 and maze[r - 1][c] == 1:
+                path.append('U')
+                backtrack(r - 1, c)
+                path.pop()
+
+            maze[r][c] = 1
+
+        if maze[0][0] == 1:
+            backtrack(0, 0)
+
+        return res
